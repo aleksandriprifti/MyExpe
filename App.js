c765416add96197f7a60c888407d85d325bc78f3
aleksandriprifti <prifti.aleksandri@gmail.com>
2024-02-16 18:12:12 +0100

    First commit

---
+++ App.js
@@ -0,0 +1,43 @@
+import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import ManageExpenses from "./screens/ManageExpenses";
+import RecentExpenses from "./screens/RecentExpenses";
+import AllExpenses from "./screens/AllExpenses";
+import { GlobalStyles } from "./constants/styles";
+
+const Stack = createNativeStackNavigator();
+const BottomTabs = createBottomTabNavigator();
+
+const ExpensesOverview = () => (
+  <BottomTabs.Navigator
+    screenOptions={{
+      headerStyle: { backgroundColor: GlobalStyles.colors.primary500 },
+      header,
+    }}
+  >
+    <BottomTabs.Screen name="RecentExpenses" component={RecentExpenses} />
+    <BottomTabs.Screen name="AllExpenses" component={AllExpenses} />
+  </BottomTabs.Navigator>
+);
+
+export default function App() {
+  return (
+    <>
+      <StatusBar style="auto" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="ExpensesOverview"
+            component={ExpensesOverview}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen name="ManageExpenses" component={ManageExpenses} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+  );
+}
